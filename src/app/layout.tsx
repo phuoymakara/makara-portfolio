148729485b55ff17084e24bd064080cda8fa232e
mark <phuoymakara@gmail.com>
2025-05-14 20:50:52 +0700

    fix: remove meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,10 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta 
-        httpEquiv="Content-Security-Policy"
-        content="script-src 'self' 'unsafe-eval'"
-      />
       <body
         //className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
